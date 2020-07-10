0c6c85ddc88900a45be9858fb4f1d9e9333abfd1
Andrey Gordeev <jgordeev@gmail.com>
2020-07-10 18:00:13 +0700

    Add webpack-dev-server and html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,24 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: './src/app/index.js',
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Development',
+            template: __dirname + '/src/index.html',
+        }),
+    ],
     output: {
         filename: 'common.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
+    watch: true,
 };
