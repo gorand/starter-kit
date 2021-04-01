f4dbd6967be4784fcdba96f6247e92629316f64f
Gordeev Andrey <jgordeev@gmail.com>
2021-04-01 13:14:05 +0700

    Add entries components

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -3,8 +3,10 @@ import ReacDOM from 'react-dom';
 import '../../scss/entry/index.scss';
 
 const App = () => (
-    <div className="box">
-        <div className="container">Typical React App</div>
+    <div className="page">
+        <div className="page__body">
+            <div className="page__title">Typical React App</div>
+        </div>
     </div>
 );
 
