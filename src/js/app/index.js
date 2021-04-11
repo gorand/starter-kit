32d1171757e57a113f9503af84a0961814e1ca6d
Gordeev Andrey <jgordeev@gmail.com>
2021-04-11 14:07:27 +0700

    Add component overspin

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -4,18 +4,25 @@ import '../../scss/entry/index.scss';
 
 const App = () => {
     const [loading, setLoading] = useState(true);
+    const [overed, setOver] = useState(false);
 
     useEffect(() => {
-        setTimeout(() => setLoading(false), 3000);
+        setTimeout(() => {
+            setLoading(false);
+            setOver(true);
+        }, 2000);
+        setTimeout(() => setOver(false), 5000);
     }, []);
 
     const spinner = <div className="spinner spinner--primary"></div>;
 
     return (
-        <div className="page">
-            <div className="page__body">
-                <div className="page__title">Typical React App</div>
-                {loading && spinner}
+        <div className={'overspin' + (overed ? ' is-visible' : '')}>
+            <div className="page">
+                <div className="page__body">
+                    <div className="page__title">Typical React App</div>
+                    {loading && spinner}
+                </div>
             </div>
         </div>
     );
