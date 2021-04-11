4571ff410110629d1e4b79e38c09c88854fc2b51
Gordeev Andrey <jgordeev@gmail.com>
2021-04-11 13:33:18 +0700

    Add component spinner

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReacDOM from 'react-dom';
 import '../../scss/entry/index.scss';
 
-const App = () => (
-    <div className="page">
-        <div className="page__body">
-            <div className="page__title">Typical React App</div>
+const App = () => {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => setLoading(false), 3000);
+    }, []);
+
+    const spinner = <div className="spinner spinner--primary"></div>;
+
+    return (
+        <div className="page">
+            <div className="page__body">
+                <div className="page__title">Typical React App</div>
+                {loading && spinner}
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 ReacDOM.render(<App />, document.getElementById('app'));
