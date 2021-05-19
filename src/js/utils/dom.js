06f72941e077aed3e341e1fc743c434ab5d283b8
Gordeev Andrey <jgordeev@gmail.com>
2021-05-19 16:24:53 +0700

    Migrate to Webpack version 5

--- src/js/utils/dom.js
+++ src/js/utils/dom.js
@@ -4,6 +4,8 @@ export const selectAll = selector => document.querySelectorAll(selector);
 
 export const getById = id => document.getElementById(id);
 
+export const selectChildById = (id, selector) => document.querySelector(`#${id} ${selector}`);
+
 export const createEl = type => document.createElement(type);
 
 export const createDiv = createEl('div');
