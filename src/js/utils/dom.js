4724b566a91ed9e0e34ec03df607e0cfb6747315
Gordeev Andrey <jgordeev@gmail.com>
2021-07-06 12:20:38 +0700

    Add new functions to utils/dom

--- src/js/utils/dom.js
+++ src/js/utils/dom.js
@@ -4,10 +4,12 @@ export const selectAll = selector => document.querySelectorAll(selector);
 
 export const getById = id => document.getElementById(id);
 
-export const selectChildById = (id, selector) => document.querySelector(`#${id} ${selector}`);
+export const selectById = (id, selector) => select(`#${id} ${selector}`);
+
+export const selectElById = (id, el) => select(`#${id} [data-el=${el}]`);
 
 export const createEl = type => document.createElement(type);
 
-export const createDiv = createEl('div');
+export const createDiv = () => createEl('div');
 
 export const getRect = el => el.getBoundingClientRect();
