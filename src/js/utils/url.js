addf534a7a8c16e3de7583302e031234323a67ae
Andrey Gordeev <jgordeev@gmail.com>
2021-09-01 22:49:33 +0700

    Add downloadFromLink to utils/url

--- src/js/utils/url.js
+++ src/js/utils/url.js
@@ -1,1 +1,10 @@
 export const pushHistory = query => window.history.pushState({ path: query }, document.title, `?${query}`);
+
+export const downloadFromLink = (path, filename) => {
+    const link = document.createElement('a');
+    link.href = path;
+    element.setAttribute('download', filename);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
